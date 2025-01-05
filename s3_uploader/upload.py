3d3cdc3c5f51ebbed276071f8b8338cfce5e80dc
TiunovNN <tiunov.li.174@gmail.com>
2025-01-05 13:39:20 +0300

    Using disk as buffer to upload.

--- s3_uploader/upload.py
+++ s3_uploader/upload.py
@@ -1,100 +1,16 @@
-import io
 import logging
 import sqlite3
 import sys
 from concurrent.futures.thread import ThreadPoolExecutor
+from tempfile import NamedTemporaryFile
 
 import boto3
 import click
 from boto3.s3.transfer import TransferConfig
 from botocore.config import Config
 from dotenv import load_dotenv
-from requests import Response, Session
+from requests import Session
 from requests.adapters import HTTPAdapter, Retry
-from s3transfer.upload import (
-    UploadFilenameInputManager,
-    UploadSeekableInputManager,
-    UploadSubmissionTask,
-)
-
-
-class FileLikeResponse(io.RawIOBase):
-    def __init__(self, resp: Response, size: int):
-        self.resp = resp
-        self.buffer = bytearray()
-        self.iterator = iter(resp.iter_content(chunk_size=10 * 2 ** 10))
-        self._tell = 0
-        self._size = size
-
-    def read(self, __size=-1):
-        while len(self.buffer) < __size and self.iterator is not None:
-            try:
-                data = next(self.iterator)
-            except StopIteration:
-                self.iterator = None
-            else:
-                self.buffer.extend(data)
-
-        result = self.buffer[:__size]
-        self.buffer = bytearray(self.buffer[__size:])
-        self._tell += len(result)
-        return result
-
-    def readable(self):
-        return True
-
-    def size(self):
-        return self._size
-
-    def tell(self):
-        return self._tell
-
-
-class UploadFileLikeResponseInputManager(UploadSeekableInputManager):
-    @classmethod
-    def is_compatible(cls, upload_source):
-        return isinstance(upload_source, FileLikeResponse)
-
-    def provide_transfer_size(self, transfer_future):
-        fileobj: FileLikeResponse = transfer_future.meta.call_args.fileobj
-        # To determine size, first determine the starting position
-        # Seek to the end and then find the difference in the length
-        # between the end and start positions.
-        transfer_future.meta.provide_transfer_size(
-            fileobj.size()
-        )
-
-    def _wrap_fileobj(self, fileobj):
-        return fileobj
-
-
-def _get_upload_input_manager_cls(self, transfer_future):
-    """Retrieves a class for managing input for an upload based on file type
-
-    :type transfer_future: s3transfer.futures.TransferFuture
-    :param transfer_future: The transfer future for the request
-
-    :rtype: class of UploadInputManager
-    :returns: The appropriate class to use for managing a specific type of
-        input for uploads.
-    """
-    upload_manager_resolver_chain = [
-        UploadFileLikeResponseInputManager,
-        UploadFilenameInputManager,
-        UploadSeekableInputManager,
-    ]
-
-    fileobj = transfer_future.meta.call_args.fileobj
-    for upload_manager_cls in upload_manager_resolver_chain:
-        if upload_manager_cls.is_compatible(fileobj):
-            return upload_manager_cls
-    raise RuntimeError(
-        f'Input {fileobj} of type: {type(fileobj)} is not supported.'
-    )
-
-
-# dirty hack
-UploadSubmissionTask._get_upload_input_manager_cls = _get_upload_input_manager_cls
 
 
 class Copier:
@@ -107,8 +23,14 @@ class Copier:
         }
     )
 
-    def __init__(self, s3_access_key_id, s3_secret_access_key, bucket):
-        self.http_client = self._create_session(192)
+    def __init__(
+        self,
+        s3_access_key_id,
+        s3_secret_access_key,
+        bucket,
+        concurrency
+    ):
+        self.http_client = self._create_session(concurrency)
         self.s3_client = boto3.client(
             's3',
             endpoint_url='https://storage.yandexcloud.net/',
@@ -120,15 +42,15 @@ class Copier:
         self.logger = logging.getLogger('Copier')
         self.transfer_config = TransferConfig(
             multipart_threshold=5 * 2 ** 30,
-            max_concurrency=2,
+            max_concurrency=concurrency * 10,
             multipart_chunksize=8 * 2 ** 20,
             use_threads=True,
         )
 
     @staticmethod
-    def _create_session(retries):
+    def _create_session(concurrency):
         retry_strategy = Retry(
-            total=retries,
+            total=15,
             status_forcelist=[429, 500, 502, 503, 504],
             allowed_methods=['HEAD', 'GET', 'OPTIONS'],
             backoff_factor=0.1
@@ -136,8 +58,8 @@ class Copier:
         session = Session()
         adapter = HTTPAdapter(
             max_retries=retry_strategy,
-            pool_maxsize=256,
-            pool_connections=256,
+            pool_maxsize=concurrency,
+            pool_connections=concurrency,
             pool_block=True,
         )
         session.mount('http://', adapter)
@@ -146,18 +68,35 @@ class Copier:
 
     def copy_file_to_s3(self, src_url, dst_path):
         self.logger.info(f'Downloading from {src_url} to {dst_path}')
-        with self.http_client.head(src_url) as response:
-            response.raise_for_status()
-            size = int(response.headers['content-length'])
-        with self.http_client.get(src_url, stream=True) as response:
-            response.raise_for_status()
-            self.s3_client.upload_fileobj(
-                FileLikeResponse(response, size),
+        try:
+            self.s3_client.head_object(
                 Bucket=self.bucket,
                 Key=dst_path,
-                ExtraArgs={"ContentType": response.headers['Content-Type']},
-                Config=self.transfer_config
             )
+            return
+        except self.s3_client.exceptions.NoSuchKey:
+            pass
+
+        with self.http_client.get(src_url, stream=True) as response:
+            response.raise_for_status()
+            tmp_context = NamedTemporaryFile(
+                buffering=10 * 2 ** 20,
+                suffix='.mkv',
+                prefix=dst_path.rpartition('/')[-1]
+            )
+            with tmp_context as tempfile:
+                for chunk in response.iter_content(8192):
+                    tempfile.write(chunk)
+
+                tempfile.flush()
+                tempfile.seek(0)
+                self.s3_client.upload_fileobj(
+                    tempfile,
+                    Bucket=self.bucket,
+                    Key=dst_path,
+                    ExtraArgs={"ContentType": response.headers['Content-Type']},
+                    Config=self.transfer_config
+                )
         self.logger.info(f'Finished uploading {src_url}')
 
     def process_item(self, item: dict[str, str]) -> tuple[str, bool]:
@@ -186,11 +125,13 @@ def configure_logging():
 @click.option('--s3-secret-access-key', type=click.STRING, required=True)
 @click.option('--bucket', type=click.STRING, required=True)
 @click.option('--db-name', type=click.STRING, default='upload_progress.db')
+@click.option('--concurrency', type=click.INT, default=1)
 def main(
     s3_access_key_id: str,
     s3_secret_access_key: str,
     bucket: str,
     db_name: str,
+    concurrency: int,
 ):
     configure_logging()
     con = sqlite3.connect(db_name)
@@ -202,7 +143,7 @@ def main(
         s3_secret_access_key=s3_secret_access_key,
         bucket=bucket,
     )
-    with ThreadPoolExecutor(max_workers=1) as executor:
+    with ThreadPoolExecutor(max_workers=concurrency) as executor:
         for result in executor.map(copier.process_item, res.fetchall()):
             src_url, success = result
             cur.execute("UPDATE upload_progress SET success = ? WHERE src = ?", (success, src_url))
